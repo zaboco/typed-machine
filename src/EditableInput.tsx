187b4176694cd6e7b28481f994c89c166a37806f
Bogdan Zaharia <zaboco@gmail.com>
2018-11-13 00:05:43 +0200

    export Fsm instead of Graph

--- src/EditableInput.tsx
+++ src/EditableInput.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { Machine, Graph, Assert, MachineTemplate } from './Machine';
+import { Machine, Assert, MachineTemplate, Fsm } from './Machine';
 import { Action, action, actionP, ActionP } from './Actions';
 
-type EditiabbleGraph = Graph<EditiabbleState, EditableMachineTemplate>;
+type EditiabbleFsm = Fsm<EditiabbleState, EditableMachineTemplate>;
 
 type EditiabbleState = 'Readonly' | 'Editing';
 
@@ -12,68 +12,68 @@ type EditableMachineTemplate = Assert<
     Readonly: {
       action: Action<'START_EDITING'>;
       model: string;
-      stateModel: ['Readonly', string];
     };
     Editing: {
       action: ActionP<'CHANGE_TEXT', string> | Action<'SAVE'> | Action<'DISCARD'>;
       model: { previous: string; draft: string };
-      stateModel: ['Editing', { previous: string; draft: string }];
     };
   }
 >;
 
-const makeEditiabbleGraph = (initialValue: string): EditiabbleGraph => ({
-  Readonly: {
-    model: initialValue,
-    render: (dispatch, model) => (
-      <div>
-        {model}
-        <button onClick={() => dispatch(action('START_EDITING'))}>Edit</button>
-      </div>
-    ),
-    transition: (action, model) => {
-      switch (action.type) {
-        case 'START_EDITING':
-          return ['Editing', { draft: model, previous: model }];
-      }
-    },
-  },
-  Editing: {
-    model: { draft: initialValue, previous: initialValue },
-    render: (dispatch, { draft }) => {
-      return (
+const makeEditiabbleFsm = (initialValue: string): EditiabbleFsm => ({
+  current: 'Readonly',
+  graph: {
+    Readonly: {
+      model: initialValue,
+      render: (dispatch, model) => (
         <div>
-          <input
-            type="text"
-            value={draft}
-            autoFocus={true}
-            onChange={ev => {
-              dispatch(actionP('CHANGE_TEXT', ev.target.value));
-            }}
-          />
-          <button onClick={() => dispatch(action('SAVE'))}>Save</button>
-          <button onClick={() => dispatch(action('DISCARD'))}>Cancel</button>
+          {model}
+          <button onClick={() => dispatch(action('START_EDITING'))}>Edit</button>
         </div>
-      );
+      ),
+      transition: (action, model) => {
+        switch (action.type) {
+          case 'START_EDITING':
+            return ['Editing', { draft: model, previous: model }];
+        }
+      },
     },
-    transition: (action, { previous, draft }) => {
-      switch (action.type) {
-        case 'SAVE':
-          return ['Readonly', draft];
-        case 'DISCARD':
-          return ['Readonly', previous];
-        case 'CHANGE_TEXT':
-          return ['Editing', { previous, draft: action.payload }];
-      }
+    Editing: {
+      model: { draft: initialValue, previous: initialValue },
+      render: (dispatch, { draft }) => {
+        return (
+          <div>
+            <input
+              type="text"
+              value={draft}
+              autoFocus={true}
+              onChange={ev => {
+                dispatch(actionP('CHANGE_TEXT', ev.target.value));
+              }}
+            />
+            <button onClick={() => dispatch(action('SAVE'))}>Save</button>
+            <button onClick={() => dispatch(action('DISCARD'))}>Cancel</button>
+          </div>
+        );
+      },
+      transition: (action, { previous, draft }) => {
+        switch (action.type) {
+          case 'SAVE':
+            return ['Readonly', draft];
+          case 'DISCARD':
+            return ['Readonly', previous];
+          case 'CHANGE_TEXT':
+            return ['Editing', { previous, draft: action.payload }];
+        }
+      },
     },
   },
 });
 
 export type EditiabbleInputProps = {
-  initial?: EditiabbleState;
   value: string;
 };
 
-export const EditiabbleInput = ({ initial = 'Readonly', value }: EditiabbleInputProps) => (
-  <Machine current={initial} graph={makeEditiabbleGraph(value)} />
+export const EditiabbleInput = ({ value }: EditiabbleInputProps) => (
+  <Machine {...makeEditiabbleFsm(value)} />
 );
--- src/Machine.ts
+++ src/Machine.ts
@@ -28,7 +28,7 @@ export class Machine<S extends string, MT extends MachineTemplate<S>> extends Re
 
 function renderCurrent<S extends string, MT extends MachineTemplate<S>>(
   fsm: Fsm<S, MT>,
-  onStateChange: ([s, m]: MT[S]['stateModel']) => void,
+  onStateChange: ([s, m]: [S, Model]) => void,
 ) {
   const node = fsm.graph[fsm.current];
 
@@ -37,30 +37,47 @@ function renderCurrent<S extends string, MT extends MachineTemplate<S>>(
   }, node.model);
 }
 
-interface Fsm<S extends string, MT extends MachineTemplate<S>> {
+export interface Fsm<S extends string, MT extends MachineTemplate<S>> {
   current: S;
   graph: Graph<S, MT>;
 }
 
-export type MachineTemplate<S extends string> = { [s in S]: NodeTemplate<s> };
+export type MachineTemplate<S extends string> = {
+  [s in S]: {
+    action: Action;
+    model: Model;
+  }
+};
 
-type NodeTemplate<S extends string = string> = {
+type Graph<S extends string, MT extends MachineTemplate<S>> = {
+  [s in S]: FsmNode<Derive<S, MT>[s], Derive<S, MT>[S]>
+};
+
+type DerivedMachineTemplate<S extends string> = { [s in S]: DerivedNodeTemplate<s> };
+
+type DerivedNodeTemplate<S extends string = string> = {
   action: Action;
   stateModel: [S, Model];
 };
 
-export type Graph<S extends string, MT extends MachineTemplate<S>> = {
-  [s in S]: FsmNode<S, MT[s], MT[S]>
-};
+type Derive<S extends string, MT extends MachineTemplate<S> = MachineTemplate<S>> = Assert<
+  DerivedMachineTemplate<S>,
+  {
+    [s in S]: {
+      action: MT[s]['action'];
+      stateModel: [s, MT[s]['model']];
+    }
+  }
+>;
 
-interface FsmNode<S extends string, CNT extends NodeTemplate<S>, NT extends NodeTemplate<S>> {
+interface FsmNode<CNT extends DerivedNodeTemplate, NT extends DerivedNodeTemplate> {
   model: GetModel<CNT>;
   transition: (a: GetAction<CNT>, m: GetModel<CNT>) => NT['stateModel'];
   render: (d: Dispatch<GetAction<CNT>>, m: GetModel<CNT>) => JSX.Element;
 }
 
-type GetModel<NT extends NodeTemplate> = Assert<Model, Second<NT['stateModel']>>;
-type GetAction<NT extends NodeTemplate> = Assert<Action, NT['action']>;
+type GetModel<NT extends DerivedNodeTemplate> = Assert<Model, Second<NT['stateModel']>>;
+type GetAction<NT extends DerivedNodeTemplate> = Assert<Action, NT['action']>;
 
 // type First<T extends [unknown, unknown]> = T[0];
 type Second<T extends [unknown, unknown]> = T[1];
