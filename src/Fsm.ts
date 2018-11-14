b43f440001652244e57493aeef6df8c7990f6c20
Bogdan Zaharia <zaboco@gmail.com>
2018-11-15 01:23:43 +0200

    make renderCurrent typesafe

--- src/Fsm.ts
+++ src/Fsm.ts
@@ -1,12 +1,5 @@
-import { Assert, Second, KeyValues } from './types/helpers';
-import {
-  ActionShape,
-  Dispatch,
-  ActionPayloads,
-  ActionHandlers,
-  Model,
-  DeriveAction,
-} from './types/Actions';
+import { Assert, Second } from './types/helpers';
+import { Dispatch, ActionPayloads, ActionHandlers, Model, DeriveAction } from './types/Actions';
 
 export function renderCurrent<S extends string, GT extends GraphTemplate<S>>(
   fsm: Fsm<S, GT>,
@@ -15,12 +8,8 @@ export function renderCurrent<S extends string, GT extends GraphTemplate<S>>(
   const node = fsm.graph[fsm.current];
 
   return node.render(action => {
-    const handler = node.actionHandlers[action[0]] as Function;
-    if (action[1] === undefined) {
-      onStateChange(handler(node.model));
-    } else {
-      onStateChange(handler(node.model, action[1]));
-    }
+    const handler = node.actionHandlers[action[0]];
+    onStateChange(handler(node.model, action[1]));
   }, node.model);
 }
 
@@ -35,7 +24,7 @@ type Graph<S extends string, GT extends GraphTemplate<S>> = { [s in S]: FsmNode<
 type FsmNode<CNT extends NodeTemplate, NT extends NodeTemplate> = {
   model: GetModel<CNT>;
   actionHandlers: ActionHandlers<NT['stateModel'], GetModel<CNT>, CNT['actionPayloads']>;
-  render: (d: Dispatch<GetAction<CNT>>, m: GetModel<CNT>) => JSX.Element;
+  render: (d: Dispatch<DeriveAction<CNT['actionPayloads']>>, m: GetModel<CNT>) => JSX.Element;
 };
 
 // === Templates ===
@@ -43,7 +32,6 @@ export type DefineTemplate<S extends string, TD extends TemplateDefinition<S>> =
   GraphTemplate<S>,
   {
     [s in S]: {
-      action: DeriveAction<TD[s]['actionPayloads']>;
       actionPayloads: TD[s]['actionPayloads'];
       stateModel: [s, TD[s]['model']];
     }
@@ -60,10 +48,8 @@ type TemplateDefinition<S extends string> = {
 export type GraphTemplate<S extends string> = { [s in S]: NodeTemplate<s> };
 
 type NodeTemplate<S extends string = string> = {
-  action: ActionShape;
   actionPayloads: ActionPayloads;
   stateModel: [S, Model];
 };
 
 type GetModel<NT extends NodeTemplate> = Assert<Model, Second<NT['stateModel']>>;
-type GetAction<NT extends NodeTemplate> = Assert<ActionShape, NT['action']>;
--- src/types/Actions.ts
+++ src/types/Actions.ts
@@ -1,17 +1,17 @@
 import { ValueOf, Assert } from './helpers';
 
-export type ActionShape = [string, unknown] | [string];
 export type Dispatch<A> = (action: A) => void;
 export type Model = Object | string | number | boolean | null;
 
 export type ActionPayloads<P = Model, T extends string = string> = { [t in T]: P };
 export type ActionHandlers<R, M, AP extends ActionPayloads = ActionPayloads> = {
-  [t in keyof AP]: ActionHandler<M, AP[t], R>
+  [t in keyof AP]: ActionHandler<R, M, AP[t]>
 };
 
-export type ActionHandler<M, P, R> = P extends null ? (m: M) => R : (m: M, p: P) => R;
+export type ActionHandler<R, M, P> = (m: M, p: P) => R;
 
 export type DeriveAction<AP extends ActionPayloads> = Assert<
   ActionShape,
   ValueOf<{ [t in keyof AP]: AP[t] extends null ? [t] : [t, AP[t]] }>
 >;
+type ActionShape = [string, unknown] | [string];
