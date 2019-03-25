00f438efe8b7135501b05e332ef65eb40c273389
Bogdan Zaharia <zaboco@gmail.com>
2019-03-25 23:49:54 +0200

    cleanup project

--- examples/react/index.tsx
+++ examples/react/index.tsx
@@ -1,26 +1,17 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import { EditableItem } from './EditableItem';
 
-import '../shared/index.css';
-import { initEditableMachine } from '../shared/EditableMachine';
+import './index.css';
 
 type AppState = {
   items: string[];
 };
 
 class App extends React.Component<{}, AppState> {
-  state: AppState = {
-    items: ['foo', 'bar', 'baz'],
-  };
-
   render() {
-    const machine = initEditableMachine('Readonly', 'Foo');
     return (
       <div className="app">
-        <h1 className="title">Editable Item</h1>
-        <EditableItem machine={machine} />
-        <EditableItem machine={machine} />
+        <h1 className="title">Demo</h1>
       </div>
     );
   }
