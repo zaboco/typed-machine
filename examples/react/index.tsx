e8916c2b1db963e67337506a64fc2be6b056129a
Bogdan Zaharia <zaboco@gmail.com>
2019-04-11 22:10:40 +0300

    add component

--- examples/react/index.tsx
+++ examples/react/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-
+import { initAppMachine } from './simple-app/App';
+import { AppComponent } from './simple-app/AppComponent';
 import './index.css';
 
 type AppState = {
@@ -9,9 +10,11 @@ type AppState = {
 
 class App extends React.Component<{}, AppState> {
   render() {
+    const appMachine = initAppMachine('LoggedOut', {});
     return (
       <div className="app">
         <h1 className="title">Demo</h1>
+        <AppComponent machine={appMachine} />
       </div>
     );
   }
--- examples/react/simple-app/AppComponent.tsx
+++ examples/react/simple-app/AppComponent.tsx
@@ -1,9 +1,53 @@
 import * as React from 'react';
+import { AppState, AppTemplate } from './App';
+import { Machine } from '../../../src/core/Machine';
+import { MachineAdapter } from '../../../src/react';
+import { LoginForm } from './LoginForm';
 
 export type AppComponentProps = {
-  
-}
+  machine: Machine<AppState, AppTemplate>;
+};
 
 export const AppComponent = (props: AppComponentProps) => (
-  //
+  <MachineAdapter<AppState, AppTemplate>
+    machine={props.machine}
+    views={{
+      LoggedOut: (dispatch, model) => (
+        <div>
+          <h2>Logged Out</h2>
+          <LoginForm
+            validNames={['JavaScripter', 'Hero']}
+            error={model.error}
+            onLoginSuccess={name => {
+              dispatch('LOGIN_SUCCESS', name);
+            }}
+            onLoginError={error => {
+              dispatch('LOGIN_ERROR', error);
+            }}
+          />
+        </div>
+      ),
+      LoggedIn: (dispatch, model) => (
+        <div>
+          <h2>Logged in</h2>
+          <span>Hello {model.name} </span>
+          <button
+            onClick={() => {
+              dispatch('NAME_TO_LOWERCASE');
+            }}
+          >
+            To lowercase
+          </button>
+          {'  '}
+          <button
+            onClick={() => {
+              dispatch('LOGOUT');
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      ),
+    }}
+  />
 );
